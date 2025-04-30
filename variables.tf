34d7e5f5d5781b1f893b7db68eeb984f7b6dcd56
Lohith <162948075+blohith@users.noreply.github.com>
2025-04-30 18:58:42 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,16 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "awsRegion" {}
-variable "instanceType" {}
-variable "instanceName" {}
+
+variable "awsRegion" {
+  description = "AWS Region for deployment"
+  type        = string
+}
+
+
+variable "instanceName" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "instanceType" {
+  description = "Type of EC2 instance"
+  type        = string
+}
